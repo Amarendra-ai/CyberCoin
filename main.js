85142c6334a27fe0e8177e3111b63d593c87574a
Amarendra Singh Yadav <82771352+Amarendra-ai@users.noreply.github.com>
2022-08-07 00:34:23 +0530

    blockchain

---
+++ main.js
@@ -0,0 +1,56 @@
+const SHA256 = require('crypto-js/sha256');
+class Block{
+    constructor(index,timestamp,data,previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = '';
+    }
+
+    calculateHash(){
+        return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+  
+    createGenesisBlock(){
+        return new Block(0,"06/08/2022","Genesis block","0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length-1];
+    }
+
+    addBlock(newBlock){
+            newBlock.previousHash = this.getLatestBlock().hash;
+            newBlock.hash = newBlock.calculateHash();
+            this.chain.push(newBlock);
+    }
+    isChainValid(){
+        for(let i=1;i<this.chain.length;i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+            if(currentBlock.hash != currentBlock.calculateHash()){
+                return false;
+            }
+            if(currentBlock.previousHash != previousBlock.hash){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+let CyberCoin = new Blockchain();
+CyberCoin.addBlock(new Block(1,"06/08/2022",{amount: 4}));
+CyberCoin.addBlock(new Block(2,"08/08/2022",{amount: 10}));
+
+console.log(JSON.stringify(CyberCoin,null,4));
+//console.log('is Blockchain valid? ' + CyberCoin.isChainValid());
+CyberCoin.chain[1].data={amount:100};
+console.log('is Blockchain valid? ' + CyberCoin.isChainValid());
