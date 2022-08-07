c8405bf115081b9038e21d4726f1269bcacaaf34
Amarendra Singh Yadav <82771352+Amarendra-ai@users.noreply.github.com>
2022-08-07 13:10:08 +0530

    added proof of work and difficulty

--- main.js
+++ main.js
@@ -5,17 +5,27 @@ class Block{
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)).toString();
+        return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)+this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0,difficulty) !== Array(difficulty+1).join("0")){
+            this.nonce++;
+            this.hash=this.calculateHash();
+        }
+        console.log("Block mined " + this.hash);
     }
 }
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 5;
     }
   
     createGenesisBlock(){
@@ -28,7 +38,7 @@ class Blockchain{
 
     addBlock(newBlock){
             newBlock.previousHash = this.getLatestBlock().hash;
-            newBlock.hash = newBlock.calculateHash();
+            newBlock.mineBlock(this.difficulty);
             this.chain.push(newBlock);
     }
     isChainValid(){
@@ -47,8 +57,11 @@ class Blockchain{
 }
 
 let CyberCoin = new Blockchain();
+console.log('Mining block 1...');
 CyberCoin.addBlock(new Block(1,"06/08/2022",{amount: 4}));
+console.log('Mining block 2...');
 CyberCoin.addBlock(new Block(2,"08/08/2022",{amount: 10}));
+
 
 console.log(JSON.stringify(CyberCoin,null,4));
 console.log('is Blockchain valid? ' + CyberCoin.isChainValid());
